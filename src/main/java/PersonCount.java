5589b4cd8940d83d4a242556381a04b90f2fc9ab
Mihails <mihailsologubs@icloud.com>
2023-10-27 12:36:06 +0300

    Second sprint project work!!!

--- src/main/java/PersonCount.java
+++ src/main/java/PersonCount.java
@@ -4,22 +4,22 @@ public class PersonCount {
 
     public int personCount(Scanner userInput) {
         int userCount;
-        int cirklStep = 0;
+        int circleStep = 0;
         System.out.println("Привет! Будем платить сообща? Введи количество участников:");
         if (!userInput.hasNextInt()) {
             System.out.println("Пожалуйста, используйте только числа при вводе!!!");
             Scanner newInput = new Scanner(System.in);
-            ++cirklStep;
+            ++circleStep;
             userCount = personCount(newInput);
         } else {
             userCount = userInput.nextInt();
         }
         if (userCount < 1) {
             System.out.println("Количество участников не может быть меньше одной персоны, попробуй снова…");
-            ++cirklStep;
+            ++circleStep;
             userCount = personCount(userInput);
         } else if (userCount == 1) {
-            if (cirklStep == 0) {
+            if (circleStep == 0) {
                 System.out.println("Счёт будет рассчитан на одну персону.");
             }
             userCount = 1;
